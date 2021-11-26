1479bfb909af4ed99fa57ee402baa0f1362a40f9
Fonkio <fonkio81@gmail.com>
2021-11-26 14:57:38 +0100

    correction while infini consommation CPU

--- src/main/java/fr/fonkio/inicium/Inicium.java
+++ src/main/java/fr/fonkio/inicium/Inicium.java
@@ -12,8 +12,11 @@ import java.io.IOException;
 import java.util.*;
 
 
-public class Inicium implements Runnable {
+public class Inicium {
+
     public static Configuration CONFIGURATION;
+    private static JDA jda;
+
     static {
         Configuration configuration = null;
         try {
@@ -23,40 +26,15 @@ public class Inicium implements Runnable {
         }
         CONFIGURATION = configuration;
     }
-    private static JDA jda;
-    private boolean running;
-    private final Scanner scanner = new Scanner(System.in);
-    private IniciumListener iniciumListener;
 
-    @Override
-    public void run() {
-        running = true;
-        while (running) {
-        }
-        scanner.close();
-        System.out.println("Le bot a été arrété.");
-        jda.shutdown();
-        System.exit(0);
-    }
 
-    public static void main(String[] args) {
-        try {
-            Inicium bot = new Inicium();
-            new Thread(bot, "boot").start();
-        } catch (LoginException | IllegalArgumentException | RateLimitedException e) {
-            e.printStackTrace();
-        }
-        CONFIGURATION.save();
-
-    }
-
-    public Inicium() throws LoginException, IllegalArgumentException, RateLimitedException {
+    public static void main(String[] args) throws LoginException {
         System.out.println("Demarrage du bot ...");
         Set<GatewayIntent> intents = new HashSet<>();
         intents.addAll(EnumSet.allOf(GatewayIntent.class));
         jda = JDABuilder.create(CONFIGURATION.getToken(),intents).setAutoReconnect(true).build();
         CONFIGURATION.save();
-        this.iniciumListener = new IniciumListener();
+        IniciumListener iniciumListener = new IniciumListener();
         jda.addEventListener(iniciumListener);
         Activity act = new Activity() {
             @Override
@@ -90,13 +68,15 @@ public class Inicium implements Runnable {
         };
         jda.getPresence().setActivity(act);
         System.out.println("Bot connecté");
+        CONFIGURATION.save();
+
     }
+
+    public Inicium() throws LoginException, IllegalArgumentException {
+
+    }
+
     public static JDA getJda() {
         return jda;
-    }
-
-
-    public void setRunning(boolean running) {
-        this.running = running;
     }
 }
