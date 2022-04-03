6e494d7efca5aa5c4a9668a2d1f33570a52b20bb
Fonkio <fonkio81@gmail.com>
2022-04-03 13:30:14 +0200

    Gestion des interdictions de la blacklist

--- src/main/java/fr/fonkio/listener/impl/EventSlashCommandInteraction.java
+++ src/main/java/fr/fonkio/listener/impl/EventSlashCommandInteraction.java
@@ -1,6 +1,8 @@
 package fr.fonkio.listener.impl;
 
 import fr.fonkio.inicium.Inicium;
+import fr.fonkio.message.EmbedGenerator;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,6 +12,21 @@ public class EventSlashCommandInteraction extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        Inicium.commands.get(event.getName()).run(event, null);
+        Guild guild = event.getGuild();
+        if (guild != null) {
+
+            if (!Inicium.CONFIGURATION.blackListContains(guild.getId(), event.getTextChannel().getId())) {
+                Inicium.commands.get(event.getName()).run(event, null);
+            } else {
+                event.replyEmbeds(
+                        EmbedGenerator.generate(
+                                event.getUser(),
+                                "Channel blacklisté",
+                                "Ce channel est dans la blacklist pour l'envoi de commande. Merci d'envoyer des commandes dans les channels prévus à cet effet")
+                                ).setEphemeral(true).queue();
+            }
+
+        }
+
     }
 }
