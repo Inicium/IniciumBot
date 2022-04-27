29b548d4a1d604b315b79634bfd02b9aa4e1cf29
Maxime <maxime.fabre10@gmail.com>
2022-04-27 21:01:50 +0200

    Improve error message when an user type /mva is not in a voice channel Close #4

--- src/main/java/fr/fonkio/command/impl/CommandMoveAll.java
+++ src/main/java/fr/fonkio/command/impl/CommandMoveAll.java
@@ -28,23 +28,28 @@ public class CommandMoveAll extends AbstractCommand {
                 if(destinationOption != null) {
                     destinationAudioChannelParameter = destinationOption.getAsAudioChannel();
                 }
-                if (destinationAudioChannelParameter != null && sourceAudioChannel != null) {
-                    if(member.hasPermission(Permission.VOICE_MOVE_OTHERS) && member.hasAccess(destinationAudioChannelParameter)) {
-                        for (Member m : sourceAudioChannel.getMembers()) {
-                            guild.moveVoiceMember(m, destinationAudioChannelParameter).queue();
+                if (destinationAudioChannelParameter != null) {
+                    if (sourceAudioChannel != null) {
+                        if(member.hasPermission(Permission.VOICE_MOVE_OTHERS) && member.hasAccess(destinationAudioChannelParameter)) {
+                            for (Member m : sourceAudioChannel.getMembers()) {
+                                guild.moveVoiceMember(m, destinationAudioChannelParameter).queue();
+                            }
+                            eventSlash.replyEmbeds(EmbedGenerator.generate(user, "\uD83D\uDE80 MoveAll", "J'ai déplacé tout le monde ! \uD83D\uDE80\u200B")).setEphemeral(true).queue();
+                        } else {
+                            eventSlash.replyEmbeds(EmbedGenerator.generate(user, "\uD83D\uDE80 MoveAll", "Tu n'as pas les permissions nécessaires pour déplacer des membres ou pour accéder à ce channel"))
+                                    .setEphemeral(true).queue();
                         }
-                        eventSlash.replyEmbeds(EmbedGenerator.generate(user, "MoveAll", "J'ai déplacé tout le monde !")).setEphemeral(true).queue();
                     } else {
-                        eventSlash.replyEmbeds(EmbedGenerator.generate(user, "MoveAll", "Tu n'as pas les permissions nécessaires pour déplacer des membres ou pour accéder à ce channel"))
+                        eventSlash.replyEmbeds(EmbedGenerator.generate(user, "\uD83D\uDE80 MoveAll", "Tu n'es pas connecté dans un channel !"))
                                 .setEphemeral(true).queue();
                     }
                 } else {
-                    eventSlash.replyEmbeds(EmbedGenerator.generate(user, "MoveAll", "Le channel est introuvable"))
+                    eventSlash.replyEmbeds(EmbedGenerator.generate(user, "\uD83D\uDE80 MoveAll", "Le channel est introuvable"))
                             .setEphemeral(true).queue();
                 }
 
             } else {
-                eventSlash.replyEmbeds(EmbedGenerator.generate(user, "MoveAll", "Tu dois être co sur un channel vocal pour demander ça."))
+                eventSlash.replyEmbeds(EmbedGenerator.generate(user, "\uD83D\uDE80MoveAll", "Tu dois être co sur un channel vocal pour demander ça."))
                         .setEphemeral(true).queue();
             }
         } else {
