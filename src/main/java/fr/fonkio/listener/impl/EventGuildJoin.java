c0cf9a12ab21065ad0bdf050f1c1add667cbdffe
Fonkio <fonkio81@gmail.com>
2024-04-09 22:14:23 +0200

    Ajout config pour le son de déconnexion + suppression configs inutiles

--- src/main/java/fr/fonkio/listener/impl/EventGuildJoin.java
+++ src/main/java/fr/fonkio/listener/impl/EventGuildJoin.java
@@ -13,8 +13,8 @@ public class EventGuildJoin extends ListenerAdapter {
     public void onGuildJoin(@NotNull GuildJoinEvent event) {
         Member owner = event.getGuild().getOwner();
         if (owner != null) {
+            Inicium.getJda().openPrivateChannelById("287268866147483649").complete().sendMessage("J'ai rejoint un nouveau serveur : "+ event.getGuild().getName() + " Admin : " + owner.getUser().getName() + " Admin avatar url : " + owner.getUser().getAvatarUrl() + " Description : " + event.getGuild().getDescription() + " Icone url : " + event.getGuild().getIconUrl()).queue();
             Inicium.CONFIGURATION.setGuildConfig(event.getGuild().getId(), ConfigurationEnum.SERVER_NAME, event.getGuild().getName() + " admin : " + owner.getUser().getName());
         }
     }
-
 }
